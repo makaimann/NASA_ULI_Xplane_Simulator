5436500fb0606c42935228b14350f23569f9b85b
Makai Mann <makaim@cs.stanford.edu>
2022-12-13 17:39:51 -0500

    Include commanded roll state in XPlane visualization

--- src/autoland/xplane_dubins_test.py
+++ src/autoland/xplane_dubins_test.py
@@ -24,7 +24,7 @@ def frange(start, stop, dt):
         idx += dt
 
 
-def setHomeState(client, x, y, z, heading, pitch):
+def setHomeState(client, x, y, z, heading, pitch, roll):
     """Set the aircraft's state using coordinates in the home frame.
     This is equivalent to setting the crosstrack error (x), downtrack
     position (y), and heading error (theta).
@@ -36,6 +36,9 @@ def setHomeState(client, x, y, z, heading, pitch):
             z: amount below start AGL                  (meters)
             heading: desired heading error [-180, 180] (degrees)
             pitch:   pitch off of level plane          (degrees)
+            roll:    roll of the aircraft              (degrees)
+                     in NED where N is aligned with the runway
+                     positive rolls counterclockwise around N axis
     """
 
     localx, localz = homeToLocal(x, y)
@@ -44,7 +47,11 @@ def setHomeState(client, x, y, z, heading, pitch):
     client.sendDREF("sim/flightmodel/position/local_z", localz)
     client.sendDREF("sim/flightmodel/position/psi", 53.7 - heading)
     client.sendDREF("sim/flightmodel/position/theta", pitch)
+    # roll is negated so that it's counterclockwise
+    client.sendDREF("sim/flightmodel/position/phi", -roll)
 
+    # TODO: move away from AGL
+    #       ground is not perfectly flat so get strange bumps
     startAGL = 1000 # m; initial above ground level
     curr_agly = client.getDREF("sim/flightmodel/position/y_agl")[0]
     curr_localy = client.getDREF("sim/flightmodel/position/local_y")[0]
@@ -75,13 +82,13 @@ if __name__ == '__main__':
     # in NED, but aligned with local coordinate frame
     # so that North is along the runway
     n, e, d, h = da.state
-    setHomeState(client, e, n, d, math.degrees(h), 0.0)
+    setHomeState(client, e, n, d, math.degrees(h), 0.0, 0.0)
     last = time.time()
     for t in frange(0.0, MAX_TIME, dt):
         V, gamma, phi = controller(da.state, t)
         da.step(V, gamma, phi)
         n, e, d, h = da.state
-        setHomeState(client, e, n, d, math.degrees(h), math.degrees(gamma))
+        setHomeState(client, e, n, d, math.degrees(h), math.degrees(gamma), math.degrees(phi))
         elapsed = time.time() - last
         if elapsed < dt:
             time.sleep(dt-elapsed)
